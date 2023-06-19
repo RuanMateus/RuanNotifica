44d029060df9d072a17607a38a3553acd764e26f
Ruan Mateus de Souza Nunes <ruanmateusbsb@gmail.com>
2023-06-19 14:12:45 -0300

    .

---
+++ RuanNotifica/RuanNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RuanNotifica
+//
+//  Created by IFBIOTIC23 on 19/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
