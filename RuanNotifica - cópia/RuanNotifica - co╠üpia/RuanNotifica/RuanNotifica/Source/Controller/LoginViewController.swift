b454c8baeb1d3ade9121644c1f164d80b9b68a8d
Ruan Mateus de Souza Nunes <ruanmateusbsb@gmail.com>
2023-06-19 18:05:48 -0300

    Add files via upload

---
+++ RuanNotifica - cópia/RuanNotifica - co╠üpia/RuanNotifica/RuanNotifica/Source/Controller/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  RuanNotifica
+//
+//  Created by Ruan Mateus on 19/06/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    var viewMain = LoginView()
+    
+    override func loadView() {
+        self.view = viewMain
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Logar"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
---
+++ RuanNotifica - cópia/RuanNotifica - co╠üpia/RuanNotifica/RuanNotifica/Source/Coordinator/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  RuanNotifica
+//
+//  Created by Ruan Mateus on 19/06/23.
+//
+
+import Foundation
+
+protocol Coordinator {
+    
+    func start ()
+}
