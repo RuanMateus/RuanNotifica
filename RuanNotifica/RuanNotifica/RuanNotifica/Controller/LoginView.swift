27970f2d85dd9458586b90c139802caff16850fd
Ruan Mateus de Souza Nunes <ruanmateusbsb@gmail.com>
2023-06-19 15:22:21 -0300

    Add files via upload

---
+++ RuanNotifica/RuanNotifica/RuanNotifica/Controller/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  RuanNotifica
+//
+//  Created by Ruan Mateus on 19/06/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
