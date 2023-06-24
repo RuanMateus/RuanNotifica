834d9b0092cb3b1c5ed73d414b0aa80bb6eddffe
Ruan Mateus de Souza Nunes <ruanmateusbsb@gmail.com>
2023-06-24 15:27:51 -0300

    Add files via upload

---
+++ RuanNotifica - Aula12/RuanNotifica - Aula12/README.md
@@ -0,0 +1,2 @@
+# RuanNotifica1
+ Aplicativo de registro e gerenciamento para o IFB
---
+++ RuanNotifica - Aula12/RuanNotifica - Aula12/RuanNotifica/RuanNotifica/Source/Controller/NovaOcorrencia/NovaOcorrenciaViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NovaOcorrenciaViewController.swift
+//  RuanNotifica
+//
+//  Created by Ruan Mateus on 23/06/23.
+//
+
+import Foundation
+import UIKit
+
+class NovaOcorrenciaViewController: ViewControllerDefault {
+    
+    //MARK: -  Clouseres
+    
+    //cria uma variável que é do
+    
+    lazy var novaOcorrenciaView: NovaOcorrenciaView = {
+        let novaOcorrenciaView = NovaOcorrenciaView()
+        
+        novaOcorrenciaView.onCameraTap = {
+            EscolherImagem().selecionadorImagem(self) { imagem in novaOcorrenciaView.setImage(image: imagem)
+            }
+        }
+        
+        return novaOcorrenciaView
+    }()
+    
+    override func loadView(){
+        self.view = novaOcorrenciaView
+    }
+    
+    // é executado quando está carregando
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Nova Ocorrência"
+    }
+}
